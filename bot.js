d9e43a7b772ffa23391ec3f4dfe14fef258edabc
blengdogamer <40428498+blengdogamer@users.noreply.github.com>
2018-08-07 22:22:45 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -210,37 +210,6 @@ if (message.content.startsWith("اسحب الجميع")) {
       
        }
          }); 
-client.on('message', message => {
-    var prefix = "$";
-if(!message.channel.guild) return;
-if(message.content.startsWith(prefix + 'move')) {
- if (message.member.hasPermission("MOVE_MEMBERS")) {
- if (message.mentions.users.size === 0) {
- return message.channel.send("``لاستخدام الأمر اكتب هذه الأمر : "``")
-}
-if (message.member.voiceChannel != null) {
- if (message.mentions.members.first().voiceChannel != null) {
- var authorchannel = message.member.voiceChannelID;
- var usermentioned = message.mentions.members.first().id;
-var embed = new Discord.RichEmbed()
- .setTitle("Succes!")
- .setColor("#000000")
- .setDescription(`لقد قمت بسحب <@${usermentioned}> الى الروم الصوتي الخاص بك✅ `)
-var embed = new Discord.RichEmbed()
-.setTitle(`You are Moved in ${message.guild.name}`)
- .setColor("RANDOM")
-.setDescription(`**<@${message.author.id}> Moved You To His Channel!\nServer --> ${message.guild.name}**`)
- message.guild.members.get(usermentioned).setVoiceChannel(authorchannel).then(m => message.channel.send(embed))
-message.guild.members.get(usermentioned).send(embed)
-} else {
-message.channel.send("``لا تستطيع سحب "+ message.mentions.members.first() +" `يجب ان يكون هذه العضو في روم صوتي`")
-}
-} else {
- message.channel.send("**``يجب ان تكون في روم صوتي لكي تقوم بسحب العضو أليك``**")
-}
-} else {
-message.react("❌")
- }}});
  client.on('guildBanAdd', function(guild) {
             const rebellog = client.channels.find("banlog", "log"),
             Onumber = 3,
@@ -596,8 +565,6 @@ client.on('message', message => {
 ❖ عدد الاعضاء
 
 ❖ $rooms
-
-❖ $move mention
 
 ❖ رابط
 
