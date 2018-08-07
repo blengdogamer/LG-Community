0f6bb9e7f7cf390aaa8f59b4f87c05b01eaca986
blengdogamer <40428498+blengdogamer@users.noreply.github.com>
2018-08-07 23:26:21 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -528,8 +528,6 @@ client.on('message', message => {
  ❖ $clear 100
 
  ❖ $delete namechannel
-
- ❖ $move
 ╔[❖════════════❖]╗
             General  Commands
 ╚[❖════════════❖]╝
@@ -567,7 +565,26 @@ client.on('message', message => {
     }
 });
 
+client.on('message', message =>{
+    let messageArray = message.content.split(" ");
+    let cmd = messageArray[0];
+    let args = messageArray.slice(1);
+    let prefix = '$';
 
+if(cmd === `${prefix}suggest`) {
+    var suggestMessage = message.content.substring(8)
+    let suggestEMBED = new Discord.RichEmbed()
+    .setColor(3447003)
+    .setTitle("New suggest just added!!")
+    .setDescription(`**${suggestMessage}**`)
+    .setFooter(`Suggested By : ${message.author.tag}`);
+    message.delete().catch(O_o=>{}) 
+    let suggests = message.guild.channels.find(`name`, "suggests");
+    if (!suggests) return message.channel.send("You should make A **suggests** channel!")
+    suggests.send(suggestEMBED);
+}
+
+});
 
 
 
