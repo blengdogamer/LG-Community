6b2186bf654ba18efb8d938cbd02a8fac54e9a23
blengdogamer <40428498+blengdogamer@users.noreply.github.com>
2018-08-08 13:03:32 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -589,7 +589,7 @@ client.on('message', message => {
             if (message.content.startsWith(prefix + "ولكم")) {
      let embed = new Discord.RichEmbed()
 .setThumbnail(message.author.avatarURL)
-.addField(' حياك الله في سيرفر يا بعد قلبي ')
+.addField(' حياك الله في سيرفر يا بعد قلبي :two_hearts: ')
 .setColor('#00000f')
   message.channel.sendEmbed(embed);
     }
